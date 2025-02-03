b321bcdde8eef5b910b4074921b03ac17084da7b
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2025-02-03 11:04:12 -0300

    🧪 test: add MethodOrderedByName with TestMethodOrder

---
+++ automated-tests-with-java-lesson/src/test/java/com/dani/JUnit_advanced_concepts/MethodOrderedByName.java
@@ -0,0 +1,31 @@
+package com.dani.JUnit_advanced_concepts;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.MethodName.class) // roda o teste sempre pela ordem do nome do método
+public class MethodOrderedByName {
+
+    @Test
+    void test3() {
+        System.out.println("Test 3");
+    }
+
+    @Test
+    void test4() {
+        System.out.println("Test 4");
+    }
+    
+    @Test
+    void test1() {
+        System.out.println("Test 1");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("Test 2");
+    }
+
+
+}
