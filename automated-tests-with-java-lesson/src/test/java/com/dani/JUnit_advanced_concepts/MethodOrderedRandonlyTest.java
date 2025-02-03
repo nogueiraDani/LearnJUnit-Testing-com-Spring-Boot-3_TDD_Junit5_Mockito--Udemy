0bd24369ea95b79eaaae594baea71a3693857a45
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2025-02-03 11:00:02 -0300

    🧪 test: add MethodOrderedRandonlyTest with TestMethodOrder

---
+++ automated-tests-with-java-lesson/src/test/java/com/dani/JUnit_advanced_concepts/MethodOrderedRandonlyTest.java
@@ -0,0 +1,29 @@
+package com.dani.JUnit_advanced_concepts;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class) // cria o teste sempre com ordem aleatória
+public class MethodOrderedRandonlyTest {
+
+    @Test
+    void test1() {
+        System.out.println("Test 1");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("Test 2");
+    }
+
+    @Test
+    void test3() {
+        System.out.println("Test 3");
+    }
+
+    @Test
+    void test4() {
+        System.out.println("Test 4");
+    }
+}
