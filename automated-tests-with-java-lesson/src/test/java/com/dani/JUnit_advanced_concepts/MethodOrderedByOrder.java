eb039c5bb45a601d22081acc035bb8fa17203ad3
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2025-02-03 11:07:55 -0300

    🧪 test: add MethodOrderedByOrder with TestMethodOrder

---
+++ automated-tests-with-java-lesson/src/test/java/com/dani/JUnit_advanced_concepts/MethodOrderedByOrder.java
@@ -0,0 +1,36 @@
+package com.dani.JUnit_advanced_concepts;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) // roda o teste sempre pela ordem do Order
+public class MethodOrderedByOrder {
+
+    @Test
+    @Order(4)
+    void test3() {
+        System.out.println("Test 3");
+    }
+
+    @Test
+    @Order(1)
+    void test4() {
+        System.out.println("Test 4");
+    }
+    
+    @Test
+    @Order(2)
+    void test1() {
+        System.out.println("Test 1");
+    }
+
+    @Test
+    @Order(3)
+    void test2() {
+        System.out.println("Test 2");
+    }
+
+
+}
