63416284c8f1501c8a8510e712b670594989676c
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2025-02-12 11:21:19 -0300

    ✨ feat: adding new project for reviewing Java Spring Boot content

---
+++ rest_with_spring_boot_and_java/src/test/AppTests.java
@@ -0,0 +1,13 @@
+package com.dani.rest_with_spring_boot_and_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
